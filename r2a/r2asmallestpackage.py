ad9ca36f6d2703cc9cf7357c6886c0d3ac607c1c
wjld <98484634+wjld@users.noreply.github.com>
2022-09-08 11:00:55 -0300

    feat: guarda simples para reduzir oscilação em redes extremamente instáveis

--- r2a/r2asmallestpackage.py
+++ r2a/r2asmallestpackage.py
@@ -1,5 +1,5 @@
-import json
 from r2a.ir2a import IR2A
+from json import load, dump
 from math import ceil
 from player.parser import parse_mpd
 
@@ -9,6 +9,9 @@ class R2ASmallestPackage(IR2A):
         self.videoRates = []
         self.worstWait = 0
         self.buffer = 0
+        self.blocked = False
+        self.prevQ = 0
+        self.max = 0
 
     def handle_xml_request(self, msg):
         self.send_down(msg)
@@ -16,17 +19,25 @@ class R2ASmallestPackage(IR2A):
     def handle_xml_response(self, msg):
         mpd = parse_mpd(msg.get_payload())
         self.videoRates = mpd.get_qi()
-        self.worstWait =  ceil(self.videoRates[-1]/self.videoRates[0])
+        self.worstWait = ceil(self.videoRates[-1]/self.videoRates[0])
         self.adjust_buffer()
         self.send_up(msg)
 
     def handle_segment_size_request(self, msg):
         minimumSize = self.videoRates[0] * self.buffer
         chosenQ = self.videoRates[0]
+        if self.blocked and minimumSize > self.max:
+            minimumSize = self.max
         for rate in self.videoRates:
             if rate > minimumSize:
                 break
             chosenQ = rate
+        if self.prevQ <= chosenQ:
+            self.prevQ = chosenQ
+        elif chosenQ == self.videoRates[0]:
+            self.max = self.prevQ
+            self.prevQ = 0
+            self.blocked = True
         msg.add_quality_id(chosenQ)
         self.send_down(msg)
 
@@ -36,10 +47,10 @@ class R2ASmallestPackage(IR2A):
     
     def adjust_buffer(self):
         with open("dash_client.json","r+") as configFile:
-            initConfig = json.load(configFile)
+            initConfig = load(configFile)
             initConfig["max_buffer_size"] = self.worstWait + 5
             configFile.seek(0)
-            json.dump(initConfig,configFile,indent=4)
+            dump(initConfig,configFile,indent=4)
             configFile.truncate()
 
     def initialize(self):
